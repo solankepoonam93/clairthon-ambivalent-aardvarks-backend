33279cb2d932b04b82206668a2f98e4b5ee046f8
KishorM <kishor.marathe@clairvoyantsoft.com>
2022-09-07 17:11:14 +0530

    initial commit for Project

---
+++ src/main/java/com/cv/srm/RnRTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cv.srm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnRTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnRTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cv/srm/RnRTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.srm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnRTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
